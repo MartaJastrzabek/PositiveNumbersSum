e61aec025f5ab95f0871c5eab55762327571f7cb
Marta <marta.jastrzabek@gmail.com>
2019-08-20 12:30:08 +0200

    PositiveNumberSum Program is reading user input until it gets negative number. Then program prints:  - entered numbers in reverse order,  - sum of entered values,  - the biggest value,  - the smallest value.

---
+++ src/PositiveNumbersSum.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class PositiveNumbersSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Wprowadź liczby: ");
+        List<Double> list = getUserInput(in);
+        printInReverseOrder(list);
+        System.out.println(getSum(list));
+        System.out.println("Największa wprowadzona wartość to : " + getMaxNumber(list));
+        System.out.println("Najmniejsza wprowadzona wartość to : " + getMinNumber(list));
+    }
+
+    private static double getMinNumber(List<Double> list) {
+        return Collections.min(list);
+    }
+
+    private static double getMaxNumber(List<Double> list) {
+        return Collections.max(list);
+    }
+
+    private static String getSum(List<Double> list) {
+        double sum = 0;
+        String elements = "";
+        for(int i = 0; i < list.size(); i++){
+            elements += list.get(i);
+            if(i < list.size()-1){
+                elements += "+";
+            }
+            sum+=list.get(i);
+        }
+        return elements + "=" + sum;
+    }
+
+    private static void printInReverseOrder(List<Double> list) {
+        for(int i = list.size()-1; i>=0; i--){
+            System.out.println(list.get(i));
+        }
+    }
+
+    private static List getUserInput(Scanner in) {
+        List<Double> list = new ArrayList<>();
+        Double input = in.nextDouble();
+        while (input >= 0){
+            list.add(input);
+            input = in.nextDouble();
+        }
+        return list;
+    }
+}
