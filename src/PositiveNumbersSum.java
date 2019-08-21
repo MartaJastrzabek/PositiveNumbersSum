4d3f134c708b6c2593c3e0b100c16fe5208f5d08
Marta <marta.jastrzabek@gmail.com>
2019-08-21 19:11:34 +0200

    PositiveNumberSum Edited version

--- src/PositiveNumbersSum.java
+++ src/PositiveNumbersSum.java
@@ -2,41 +2,33 @@ import java.util.*;
 
 public class PositiveNumbersSum {
     public static void main(String[] args) {
-                System.out.println("Wprowadź liczby: ");
-        try (Scanner in = new Scanner(System.in)){
+        System.out.println("Wprowadź liczby: ");
+        try (Scanner in = new Scanner(System.in)) {
             List<Double> list = getUserInput(in);
             printInReverseOrder(list);
             System.out.println(getSum(list));
-            System.out.println("Największa wprowadzona wartość to : " + getMaxNumber(list));
-            System.out.println("Najmniejsza wprowadzona wartość to : " + getMinNumber(list));
-        } catch (InputMismatchException e){
+            System.out.println("Największa wprowadzona wartość to : " + Collections.min(list));
+            System.out.println("Najmniejsza wprowadzona wartość to : " + Collections.max(list));
+        } catch (InputMismatchException e) {
             System.out.println("Wprowadzono złą wartość");
         }
-    }
-
-    private static double getMinNumber(List<Double> list) {
-        return Collections.min(list);
-    }
-
-    private static double getMaxNumber(List<Double> list) {
-        return Collections.max(list);
     }
 
     private static String getSum(List<Double> list) {
         double sum = 0;
         String elements = "";
-        for(int i = 0; i < list.size(); i++){
+        for (int i = 0; i < list.size(); i++) {
             elements += list.get(i);
-            if(i < list.size()-1){
+            if (i < list.size() - 1) {
                 elements += "+";
             }
-            sum+=list.get(i);
+            sum += list.get(i);
         }
         return elements + "=" + sum;
     }
 
     private static void printInReverseOrder(List<Double> list) {
-        for(int i = list.size()-1; i>=0; i--){
+        for (int i = list.size() - 1; i >= 0; i--) {
             System.out.println(list.get(i));
         }
     }
@@ -44,7 +36,7 @@ public class PositiveNumbersSum {
     private static List getUserInput(Scanner in) {
         List<Double> list = new ArrayList<>();
         Double input = in.nextDouble();
-        while (input >= 0){
+        while (input >= 0) {
             list.add(input);
             input = in.nextDouble();
         }
